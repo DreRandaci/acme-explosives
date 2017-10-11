f42f731d85272b820fd63c407e5a325220931172
Dre mf Randaci <drerandaci@gmail.com>
2017-10-11 15:47:27 -0500

    two promises consoling out

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,1 +1,86 @@
 'use strict';
+
+const dom = require('./domHandler');
+
+let products = [];
+
+const categoriesJSON = () => {
+    return new Promise((resolve, reject) => {
+        $.ajax("./db/categories.json").done((data) => {
+            resolve(data.categories);    
+        }).fail((error) => {
+            reject(error);
+        });
+    });
+};
+
+const typesJSON = () => {
+    return new Promise((resolve, reject) => {
+        $.ajax("./db/types.json").done((data) => {
+            resolve(data.types);    
+        }).fail((error) => {
+            reject(error);
+        });
+    });
+};
+
+const productsJSON = () => {
+    return new Promise((resolve, reject) => {
+        $.ajax("./db/products.json").done((data) => {
+            resolve(data.products);    
+        }).fail((error) => {
+            reject(error);
+        });
+    });
+};
+
+// const allTheCats = () => {
+//     return new Promise((resolve, reject) => {
+//         $.ajax("./db/catSnacks.json").done((catData) => {
+//             resolve(catData.cats);    
+//         }).fail((error) => {
+//             reject(error);
+//         });
+//     });
+// };
+
+const dataGetter = () => {
+    Promise.all([categoriesJSON(), typesJSON()]).then((results) => {
+        console.log('results in productGetter:', results);
+        // allTheCats().then((cats) => {
+        results.forEach((array) => {
+            console.log('full array in first forEach loop:', array);
+            array.forEach((eachArrayIndex) => {
+                console.log('eachArrayIndex in second forEach loop:', eachArrayIndex);
+                // thing.snacks = [];
+                // types.forEach((products) => {
+                //     console.log('products in third forEach loop', products);
+        //             cats.forEach((cat) => {
+        //                 if (cat.id === catId) {
+        //                     dino.snacks.push(cat);
+        //                 }
+        //             });
+                // });
+        //         dinosaurs.push(dino);
+    });
+            });
+        // makeProducts();
+    // });        
+    }).catch((error) => {
+        console.log('error from Promise.all', error);
+    });
+};
+
+const makeProducts = () => {
+    // dom(products);
+};
+
+const initializer = () => {
+    dataGetter();
+};
+
+const getProducts = () => {
+    return products;
+};
+
+module.exports = {initializer, getProducts};
