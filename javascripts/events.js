ae9e7aeb25bfd1b4b3805a0ff1f16abef0e1b279
Dre mf Randaci <drerandaci@gmail.com>
2017-10-16 18:26:45 -0500

    added card styles

--- javascripts/events.js
+++ javascripts/events.js
@@ -4,8 +4,9 @@ const data = require('./data');
 const dom = require ('./domHandler');
 
 $('#dropdown').click((e) => {
+    //grab all the data, pass it to domBuilder
     let allData = data.getAllData();
-        dom.buildDom(allData, e);
+        dom.buildDom(allData, e);        
 });
 
 module.exports = {};
