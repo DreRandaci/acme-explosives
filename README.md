e936993428b270c6b72e3ef48e56944f03f092a4
Dre mf Randaci <drerandaci@gmail.com>
2017-10-15 22:52:59 -0500

    all products printing to page

--- README.md
+++ README.md
@@ -72,7 +72,7 @@ products.json
 
 - [x] Create a simple user interface for your product catalog where a user can select a category from a dropdown. 
 - [x] When a category is selected, you must use Promises to read, first, from the categories.json to load that array of objects, then load types.json, then products.json.
-- [] Once all data is loaded, you need to display the products in a Bootstrap grid. 
-- [] Each product must display the string name of its product type, and product category. Not the integer id value.
+- [x] Once all data is loaded, you need to display the products in a Bootstrap grid. 
+- [x] Each product must display the string name of its product type, and product category. Not the integer id value.
 
 ![GitHub Logo](https://user-images.githubusercontent.com/30006986/31524481-8d567fb6-af7f-11e7-8d31-af9c388ed26e.png)
--- javascripts/domHandler.js
+++ javascripts/domHandler.js
@@ -1,7 +1,27 @@
 'use strict';
 
-const buildDom = (thing) => {
-    console.log('thing in build dom:', thing);
+const buildDom = (allData, e) => {
+    $('#output').html('');
+    let domString = '';
+    allData.forEach((thing) => {
+        if (e.target.innerHTML === thing[0].name) {                        
+            domString += `<h1>${thing[0].name}</h1>`;
+            thing[0].categoryTypes.forEach((types) => {                
+                domString += `<div class='col-md-4'>`;
+                domString += `<h3>${types.name}</h3>`;
+                thing[0].products.forEach((prod) => {
+                    domString += `<p>${prod[0].name}</p>`;  
+                    domString += `</div>`;
+                });
+            });
+            
+        }        
+    });
+    writeToDom(domString);
+};
+
+const writeToDom = (str) => {
+    $('#output').append(str);
 };
 
 module.exports = {buildDom};
